d1720fae8b802417a84b0161538c09e534a01cad
Bryan Montoya Osorio <42284607+bryanMontoya@users.noreply.github.com>
2021-05-14 16:07:11 -0500

    Se borran credenciales.

--- src/Credentials.h
+++ src/Credentials.h
@@ -2,20 +2,20 @@
 #define ARDUINO_CREDENTIALS_H
 
 /* WiFi Credentials to connect Internet */
-#define STA_SSID "Montoya_Osorio"
-#define STA_PASS "funnyfoca00"
+#define STA_SSID "xxxx"
+#define STA_PASS "xxxx"
 
 /* Provide MQTT broker credentials as denoted in maqiatto.com. */
-#define MQTT_BROKER       "maqiatto.com"
+#define MQTT_BROKER       "xxxx"
 #define MQTT_BROKER_PORT  1883
-#define MQTT_USERNAME     "bmontoyaosorios@gmail.com"
-#define MQTT_KEY          "funnyfoca00"
+#define MQTT_USERNAME     "xxxx"
+#define MQTT_KEY          "xxxx"
 
 /* Provide topic as it is denoted in your topic list. 
  * For example mine is : cadominna@gmail.com/topic1
  * To add topics, see https://www.maqiatto.com/configure
  */
-#define TOPICESTADO            "bmontoyaosorios@gmail.com/Estado"
-#define TOPICTEMPERATURE       "bmontoyaosorios@gmail.com/Temperatura"
+#define TOPICESTADO            "xxxx/Estado"
+#define TOPICTEMPERATURE       "xxxx/Temperatura"
 
 #endif /* ARDUINO_CREDENTIALS_H */
